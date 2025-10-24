a966b83d5ed322aedf4a8884c095c0db017e7911
Liriel <lirielcastroreis@gmail.com>
2025-10-24 20:08:35 -0300

    Preparar para deploy DigitalOcean

--- config/puma.rb
+++ config/puma.rb
@@ -2,11 +2,16 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
+port ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
-# Worker mode is not supported on Windows
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# preload_app!
+# Worker mode for production (não use no Windows local)
+if ENV.fetch("RAILS_ENV") { "development" } == "production"
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+  preload_app!
+end
+
+plugin :tmp_restart
 
